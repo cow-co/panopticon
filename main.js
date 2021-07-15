9eaf2419240975e080148ec4c9824e9b226aa8b7
Cow-Co <cow-co@users.noreply.github.com>
2021-07-15 22:13:30 +0100

    Open the Electron Window

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require("electron");
+
+createWindow = () => {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    window.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
