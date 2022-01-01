2bfc2a2187256b339703c7b14e74e940622d908d
cow <cow-co@users.noreply.github.com>
2022-01-01 13:42:10 +0000

    Add Prod Config

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,16 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      entities: []
+    }
+  }
+}
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
