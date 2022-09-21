9d8518c9b54f2072f2874258fc76149a7affe30e
cow <cow-co@users.noreply.github.com>
2022-09-21 21:49:20 +0100

    Proper Paging, and Delete Entities

--- src/components/layout/VisualisationPanel.jsx
+++ src/components/layout/VisualisationPanel.jsx
@@ -1,14 +1,26 @@
 // This is the layout element in which the main view sits
 import { Component } from "react";
 import Pagination from '@mui/material/Pagination';
+import Stack from "@mui/material/Stack";
+import EntityListItem from "../../visualisation/EntityListItem";
 
 class VisualisationPanel extends Component {
   constructor() {
     super();
     this.state = {
-      page: 1
+      page: 1,
+      entities: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6"
+      ],
+      pageSize: 5
     };
     this.handlePageChange = this.handlePageChange.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   handlePageChange(event, value) {
@@ -18,14 +30,34 @@ class VisualisationPanel extends Component {
     });
   }
 
+  handleDelete(entityName) {
+    const idx = this.state.entities.indexOf(entityName);
+    const ents = this.state.entities.slice();
+    ents.splice(idx, 1);
+    this.setState({
+      ...this.state,
+      entities: ents
+    });
+  }
+
   render() {
+    const start = (this.state.page - 1) * this.state.pageSize;
+    const end = start + this.state.pageSize;
+    const entitiesInPage = this.state.entities.slice(start, end);
+    const numPages = Math.ceil(this.state.entities.length / this.state.pageSize);
+
     // As a first pass, we're just making it a simple list of
     // entities
     // TODO Make this into a network graph later on
     return (
       <div className="vis-panel">
-        <h2>{this.state.page}</h2>
-        <Pagination count={5} variant="outlined" color="primary" page={this.state.page} onChange={this.handlePageChange} />
+        <Pagination count={numPages} variant="outlined" color="primary" page={this.state.page} onChange={this.handlePageChange} />
+        <Stack>
+          {entitiesInPage.map(entity => {
+            console.log(entity);
+            return <EntityListItem key={entity} entityName={entity} onDelete={this.handleDelete} />
+          })}
+        </Stack>
       </div>
     );
   }
